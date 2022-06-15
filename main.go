dbb0d837aa93e3acf4e0219b0d3e31cef5cd0714
creatorQuen <spastime@yandex.ru>
2022-06-15 11:41:08 +0300

    feat: getAllCustomers and add header to route, tag for json

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
+
 func main() {
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	http.ListenAndServe("localhost:8000", nil)
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Anohin", "New Delhi", "110075"},
+		{"Rob", "New Delhi", "110075"},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(customers)
 }
