6a2607d49fdf70fc9bb05e29358da4b5947b834b
creatorQuen <spastime@yandex.ru>
2022-06-16 19:00:08 +0300

    feat: refactoring error handlers

--- app/customerHandlers.go
+++ app/customerHandlers.go
@@ -4,16 +4,9 @@ import (
 	"ashishi-banking/service"
 	"encoding/json"
 	"encoding/xml"
-	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
-
-type Customer struct {
-	Name    string `json:"full_name" xml:"name"`
-	City    string `json:"city" xml:"city"`
-	Zipcode string `json:"zip_code" xml:"zipcode"`
-}
 
 type CustomerHandler struct {
 	service service.CustomerService
@@ -36,28 +29,23 @@ func (ch *CustomerHandler) getAllCustomers(w http.ResponseWriter, r *http.Reques
 	}
 }
 
-//func greet(w http.ResponseWriter, r *http.Request) {
-//	fmt.Fprint(w, "Hello World!!")
-//}
-
-//func (ch *CustomerHandler) simpleHandler(c context.Context, next http.Handler) http.Handler {
-//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//		// FIXME Do something with our context
-//		//next.ServeHTTP(w, r)
-//	})
-//}
-
 func (ch *CustomerHandler) getCustomer(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["customer_id"]
 
 	customer, err := ch.service.GetCustomer(id)
 	if err != nil {
-		w.WriteHeader(err.Code)
-		fmt.Fprint(w, err.Message)
-		return
+		writeResponse(w, err.Code, err.AsMessage())
+	} else {
+		writeResponse(w, http.StatusOK, customer)
 	}
 
+}
+
+func writeResponse(w http.ResponseWriter, code int, data interface{}) {
 	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(customer)
+	w.WriteHeader(code)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		panic(err)
+	}
 }
